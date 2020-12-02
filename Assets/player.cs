add35a8700a9e27be360ddb07a54482d704b720a
hukopo <hukopo@yandex.ru>
2020-12-02 23:08:05 +0500

    prijok

--- Assets/player.cs
+++ Assets/player.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class player : MonoBehaviour
 {
@@ -6,19 +7,72 @@ public class player : MonoBehaviour
     public float jumpForce;
 
     public Rigidbody2D rb2d;
+    public bool facingRight = true;
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    public int maxExtraJump;
+
+    private int extraJump;
+
+    private void Start()
+    {
+        extraJump = maxExtraJump;
+    }
 
     void FixedUpdate()
     {
         var moveHorizontal = Input.GetAxis("Horizontal");
 
+        var movement = new Vector2(moveHorizontal, 0);
+        rb2d.AddForce(movement * speed);
+
+
+        if ((!facingRight && moveHorizontal > 0) || (facingRight && moveHorizontal < 0))
+        {
+            Flip();
+        }
+
+        UpdateJumpState();
+    }
+
+    private void UpdateJumpState()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
         var moveVertical = Input.GetAxis("Vertical");
 
-        var movement = new Vector2(moveHorizontal, 0);
-
-        rb2d.AddForce(movement * speed);
-        if (moveVertical > 0)
+        if (isGrounded)
         {
-            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            extraJump = maxExtraJump;
         }
+
+        if (moveVertical > 0 && extraJump > 0)
+        {
+            rb2d.velocity = Vector2.up * jumpForce;
+            extraJump--;
+        }
+        else if (moveVertical > 0 && extraJump == 0 && isGrounded)
+        {
+            rb2d.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        var scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+
+
+    void Jump()
+    {
+        facingRight = !facingRight;
+        var scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
     }
 }
