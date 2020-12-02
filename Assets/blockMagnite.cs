47ee6ce19af2f88cfdef4d54a559c9e550e8861c
hukopo <hukopo@yandex.ru>
2020-12-02 21:31:31 +0500

    init

---
+++ Assets/blockMagnite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class blockMagnite : MonoBehaviour
+{
+    public GameObject player;
+    public Rigidbody2D rb2d;
+    public float speed;
+    public int count;
+
+    void FixedUpdate()
+    {
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(1, 0);
+        count++;
+        Console.WriteLine(count);
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb2d.AddForce(player.transform.position - transform.position * speed);
+    }
+}
