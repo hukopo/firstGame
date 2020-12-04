c4fb9e886244961410c32463338c1d83b2d6c018
hukopo <hukopo@yandex.ru>
2020-12-04 20:24:49 +0500

    угарный ворон

---
+++ Assets/Crow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Crow : MonoBehaviour
+{
+    public GameObject player;
+    public Rigidbody2D rb2d;
+    public float speed;
+    private bool facingRight = true;
+
+    void FixedUpdate()
+    {
+        rb2d.AddForce(player.transform.position - transform.position * speed);
+
+        if (player.transform.position.x - transform.position.x > 0 && facingRight ||
+            player.transform.position.x - transform.position.x < 0 && !facingRight)
+        {
+            Flip();
+        }
+    }
+
+    protected void Flip()
+    {
+        facingRight = !facingRight;
+        var scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+}
