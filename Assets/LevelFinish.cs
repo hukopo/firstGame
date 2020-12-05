e2c54bd004e8ee98ffa72617c914a4c7b22be7b0
hukopo <hukopo@yandex.ru>
2020-12-05 20:26:46 +0500

    fix

--- Assets/LevelFinish.cs
+++ Assets/LevelFinish.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,8 +7,14 @@ public class LevelFinish : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
-		var btn = gameObject.GetComponent<Button>();
-		btn.onClick.AddListener(TaskOnClick);
+		try
+		{
+			var btn = gameObject.GetComponent<Button>();
+			btn.onClick.AddListener(TaskOnClick);
+		}
+		catch (Exception e)
+		{
+		}
 	}
 
 	void TaskOnClick()
