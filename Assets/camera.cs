90c44a5f1aca1983764c9fc092e711b2a26d16a6
hukopo <hukopo@yandex.ru>
2020-12-07 21:23:02 +0500

    camera moving

--- Assets/camera.cs
+++ Assets/camera.cs
@@ -1,24 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class camera : MonoBehaviour
 {
-    public GameObject player;        //Public variable to store a reference to the player game object
+    public float damping = 1.5f;
+    public Vector2 offset = new Vector2(2f, 1f);
+    public bool faceLeft;
+    private Transform player;
+    private int lastX;
 
-    private Vector3 offset;            //Private variable to store the offset distance between the player and camera
-
-    // Use this for initialization
-    void Start () 
+    void Start()
     {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
+        offset = new Vector2(Mathf.Abs(offset.x), offset.y);
+        FindPlayer(faceLeft);
     }
 
-    // LateUpdate is called after Update each frame
-    void LateUpdate () 
+    public void FindPlayer(bool playerFaceLeft)
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        lastX = Mathf.RoundToInt(player.position.x);
+        if (playerFaceLeft)
+        {
+            transform.position = new Vector3(player.position.x - offset.x, player.position.y + offset.y,
+                transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y,
+                transform.position.z);
+        }
+    }
+
+    void Update()
+    {
+        if (player)
+        {
+            int currentX = Mathf.RoundToInt(player.position.x);
+            if (currentX > lastX) faceLeft = false;
+            else if (currentX < lastX) faceLeft = true;
+            lastX = Mathf.RoundToInt(player.position.x);
+
+            Vector3 target;
+            if (faceLeft)
+            {
+                target = new Vector3(player.position.x - offset.x, player.position.y + offset.y, transform.position.z);
+            }
+            else
+            {
+                target = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
+            }
+
+            Vector3 currentPosition = Vector3.Lerp(transform.position, target, damping * Time.deltaTime);
+            transform.position = currentPosition;
+        }
     }
 }
